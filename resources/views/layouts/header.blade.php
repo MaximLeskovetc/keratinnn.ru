c80ebb11d054585022fbefc366c0682c9858cade
LeskovetcMaxim <leskovstudio@gmail.com>
2018-01-14 00:03:24 +0300

    Обновление шапки

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,17 @@
-<nav class="navbar navbar-light bg-light navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="{{url('/')}}"><b>Keratin</b>NN</a>
-        <ul class="pull-right list-inline">
+        <a class="navbar-brand" href="{{url('/')}}">Keratin<b>NN</b></a>
+        <ul class="contact pull-right list-inline">
             <li class="list-inline-item m-l-15">
-                <i class="fa fa-home"></i>Нижний Новгород</li>
+                <i class="fa fa-map-marker"></i> Нижний Новгород
+            </li>
             <li class="list-inline-item m-l-15">
                 <a href="tel:8(908)157-49-97">
-                    <i class="fa fa-phone"></i>8(908)157-49-97
+                    <i class="fa fa-phone"></i> 8(908)157-49-97
                 </a>
             </li>
             <li class="list-inline-item m-l-15">
-                <a href="https://vk.com/lina_leskovets">
+                <a href="https://vk.com/keratin_botox_nn">
                     <i class="fa fa-vk"></i>
                 </a>
             </li>
