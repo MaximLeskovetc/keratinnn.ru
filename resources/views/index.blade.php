229f0c5a591c5c219b7703838a5d57d4bbe9206f
LeskovetcMaxim <leskovstudio@gmail.com>
2018-01-22 21:51:08 +0300

    Обновление дизайна

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,19 +4,20 @@
         <div class="container">
             <h1>KeratinNN</h1>
             <h3>Мастер по восстановлению волос</h3>
-            <a href="#write" type="submit" class="btn btn-lg btn-success">Записаться сейчас</a>
+            <a href="#write" type="submit" class="btn btn-md btn-success">Записаться сейчас</a>
         </div>
     </header>
     <section class="showcase mt-5">
         <div class="container-fluid">
             <div class="service">
                 <div class="row">
-                    <div class="col-lg-6">
-                        <div class="service-image" style="background-image: url({{url('/images/upload/keratin.jpg')}}">
+                    <div class="col-md-6">
+                        <div class="service-image">
+                            <img class="img-fluid" src="{{url('/images/upload/keratin.jpg')}}"/>
                             <h2>Кератиновое выпрямление</h2>
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-md-6">
                         <p> Кератиновое выпрямление и восстановление - одна из самых популярных процедур для
                             волос.
                             Блестящие,идеально прямые и здоровые волосы - вот результат,который не заставит себя
@@ -34,12 +35,13 @@
             </div>
             <div class="service">
                 <div class="row">
-                    <div class="col-lg-6">
-                        <div class="service-image" style="background-image: url({{url('/images/upload/polir.jpg')}}">
+                    <div class="col-md-6">
+                        <div class="service-image">
+                            <img class="img-fluid" src="{{url('/images/upload/polir.jpg')}}">
                             <h2>Полировка волос</h2>
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-md-6">
                         <p> Полировка волос - замечательная альтернатива "горячим ножницам".
                             Наверно,каждая девушка уже сталкивалась с такой проблемой,как секущиеся кончики. Они не
                             дают
@@ -59,12 +61,13 @@
             </div>
             <div class="service">
                 <div class="row">
-                    <div class="col-lg-6">
-                        <div class="service-image" style="background-image: url({{url('/images/upload/botox.jpg')}}">
+                    <div class="col-md-6">
+                        <div class="service-image">
+                            <img class="img-fluid" src="{{url('/images/upload/botox.jpg')}}">
                             <h2>Ботокс волос</h2>
                         </div>
                     </div>
-                    <div class="col-lg-6">
+                    <div class="col-md-6">
                         <p> Ботокс для волос - новинка в сфере ухода за волосами. Несмотря на название,данная
                             процедура
                             не
@@ -81,40 +84,13 @@
                     </div>
                 </div>
             </div>
-            <div class="service">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="service-image" style="background-image: url({{url('/images/upload/flising.jpg')}}">
-                            <h2>Флисинг</h2>
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <p> Флисинг - это долговременный прикорневой объем. Часто его путают с процедурой "Буст Ап".
-                            Но
-                            технология процедуры совершенно другая : объем создается при помощи "начеса" у
-                            корней,фиксируется
-                            специальным составом,спустя время выдержки "начес" мягко разбирается с помощью
-                            бальзама,после
-                            волосы
-                            укладываются как обычно.При этом волосы сохраняют форму и объем на длительный срок.
-                            Никаких
-                            шпилек и
-                            бигуди! Никаких заломов,"гофре" и кудряшек! Только естественный объем с сохранением
-                            структуры
-                            волоса! </p>
-                        <a href="{{url('/service/flising')}}" class="btn btn-info mt-3">Подробнее</a>
-                        <a href="#write" class="btn btn-success mt-3">Записаться</a>
-                    </div>
-                </div>
-            </div>
         </div>
     </section>
     <section class="text-center">
         <div class="container">
-            <div class="col-lg-12">
+            <div class="col-md-12">
                 <div class="title">Интересные статьи</div>
                 <div class="row">
-                    <div class="clearfix"></div>
                     @foreach($posts as $post)
                         <div class="col-lg-6">
                             <div class="post-image"
@@ -134,7 +110,7 @@
             <div class="title">Примеры работ</div>
             <div class="row portfolio">
                 @foreach($portfolios as $portfolio)
-                    <div class="col-lg-6">
+                    <div class="col-md-6">
                         <img class="img-fluid" src="{{url('images/upload')}}/{{$portfolio->image}}"/>
                     </div>
                 @endforeach
@@ -149,20 +125,20 @@
                 <div class="col-xl-9 mx-auto">
                     <h2 id="write" class="mb-4">Запишись прямо сейчас и получи подарок!</h2>
                 </div>
-                <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                <div class="col-md-10 col-md-8 col-xl-7 mx-auto">
                     <form method="POST" enctype="multipart/form-data"
                           action="{{action('ApplicationController@store')}}">
                         <input type="hidden" name="_token" value="{{csrf_token()}}"/>
                         <div class="form-row">
                             <div class="col-12 col-md-9 mb-2 mb-md-0">
-                                <input id="phone" name="phone" type="text" class="form-control form-control-lg"
+                                <input id="phone" name="phone" type="text" class="form-control form-control-md"
                                        placeholder="Ваш номер телефона">
                             </div>
                             @foreach ($errors->all() as $error)
                                 <li>{{ $error }}</li>
                             @endforeach
                             <div class="col-12 col-md-3">
-                                <button type="submit" class="btn btn-lg btn-success">Оставить заявку!</button>
+                                <button type="submit" class="btn btn-md btn-success">Оставить заявку!</button>
                             </div>
                         </div>
                     </form>
