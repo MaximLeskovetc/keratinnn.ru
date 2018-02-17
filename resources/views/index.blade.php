5bf1734fb9d7bcf0c23d83f2157edbc3102da93b
LeskovetcMaxim <leskovstudio@gmail.com>
2018-02-17 12:57:47 +0300

    Изменения в дизайне

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -98,7 +98,7 @@
                         <div class="col-lg-6">
                             <div class="post-image"
                                  style="background-image: url({{url('images/upload/post')}}/{{$post->image}})">
-                                <a href="{{url('/post')}}/{{$post->alias}}">
+                                <a class="mb-3" href="{{url('/post')}}/{{$post->alias}}">
                                     {{$post->title}}
                                 </a>
                             </div>
