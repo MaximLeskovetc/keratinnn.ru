c26abc57b30f1ed533c4499a3b4c5fa8cc32d19d
MaximLeskovets <leskovstudio@gmail.com>
2018-12-02 18:46:09 +0300

    Новый адрес салона

--- resources/views/post/view.blade.php
+++ resources/views/post/view.blade.php
@@ -6,22 +6,15 @@
     Обзор статьи {{$post->title}}
 @endsection
 @section('content')
-    {{--<div class="container">--}}
-        {{--<div class="row mt-5">--}}
-            {{--<img style="max-height: 400px" class="img-fluid" src="{{asset('/images/upload/post')}}/{{$post->image}}"--}}
-                 {{--alt="{{$post->title}}">--}}
-            {{--<h1>{{$post->title}}</h1>--}}
-            {{--<p class="lead mb-3">{!!$post->description!!}</p>--}}
-            {{--<a href="{{url('/')}}" class="btn btn-info mb-3">На главную</a>--}}
-        {{--</div>--}}
-    {{--</div>--}}
     <div class="container">
         <div class="row mt-5">
             <div class="post-img" style="background-image: url({{asset('/images/upload/post')}}/{{$post->image}})">
                 <h1>{{$post->title}}</h1>
             </div>
             <p class="lead mb-3">{!!$post->description!!}</p>
-            <a href="{{url('/')}}" class="btn btn-primary mb-3">На главную</a>
+            <p>
+                <a href="{{url('/')}}" class="btn btn-primary mb-3">На главную</a>
+            </p>
         </div>
     </div>
 @endsection
